4b4e9cbb2012f1aab075665addfb974fbda86520
Minami Tomonobu <minami@edanoue.com>
2022-12-04 01:19:48 +0900

    Add new Velocity interface

---
+++ Runtime/Interfaces/IHasVelocity.cs
@@ -0,0 +1,10 @@
+﻿namespace Edanoue.VR.Device.Core
+{
+    public interface IHasVelocity
+    {
+        public (float X, float Y, float Z) LinearVelocity { get; }
+        public (float X, float Y, float Z) LinearAcceleration { get; }
+        public (float X, float Y, float Z) AngularVelocity { get; }
+        public (float X, float Y, float Z) AngularAcceleration { get; }
+    }
+}
