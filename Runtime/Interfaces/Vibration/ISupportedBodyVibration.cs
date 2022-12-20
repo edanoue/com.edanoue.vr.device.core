5703e7e0ae3a4631d8d977c02ae4897aa6fa07c8
Minami Tomonobu <minami@edanoue.com>
2022-12-20 19:06:30 +0900

    :sparkles: Oculus の実装をもとに3種類の振動のインタフェースを追加

---
+++ Runtime/Interfaces/Vibration/ISupportedBodyVibration.cs
@@ -0,0 +1,19 @@
+﻿// Copyright Edanoue, Inc. All Rights Reserved.
+
+namespace Edanoue.VR.Device.Core
+{
+    public interface ISupportedBodyVibration
+    {
+        /// <summary>
+        ///     本体 を振動させます
+        /// </summary>
+        /// <remarks>
+        ///     確認済みの対応機種
+        ///     - Oculus Quest 2 Touch Controller
+        ///     - Meta Quest Pro Controller
+        /// </remarks>
+        /// <param name="frequency"></param>
+        /// <param name="amplitude"></param>
+        void SetVibration(float frequency, float amplitude);
+    }
+}
---
+++ Runtime/Interfaces/Vibration/ISupportedTriggerVibration.cs
@@ -0,0 +1,18 @@
+﻿// Copyright Edanoue, Inc. All Rights Reserved.
+
+namespace Edanoue.VR.Device.Core
+{
+    public interface ISupportedTriggerVibration
+    {
+        /// <summary>
+        ///     人差し指 (Trigger) を振動させます
+        /// </summary>
+        /// <remarks>
+        ///     確認済みの対応機種
+        ///     - Meta Quest Pro Controller
+        /// </remarks>
+        /// <param name="frequency"></param>
+        /// <param name="amplitude"></param>
+        void SetVibration(float frequency, float amplitude);
+    }
+}
