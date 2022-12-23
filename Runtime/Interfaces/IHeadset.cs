608a2818aedad5605a846dca4a23493f6c284b99
Minami Tomonobu <minami@edanoue.com>
2022-12-23 15:29:07 +0900

    :sparkles: Support InputFocus state and event.

--- Runtime/Interfaces/IHeadset.cs
+++ Runtime/Interfaces/IHeadset.cs
@@ -13,11 +13,36 @@ namespace Edanoue.VR.Device.Core
         public bool IsMounted { get; }
 
         /// <summary>
+        ///     ユーザーが現在アプリケーションに集中しているかどうかを取得.
+        /// </summary>
+        public bool HasInputFocus { get; }
+
+        /// <summary>
         /// </summary>
         public event Action Mounted;
 
         /// <summary>
         /// </summary>
         public event Action Unmounted;
+
+        /// <summary>
+        ///     ユーザーがアプリケーションに集中しなくなったときに呼ばれます
+        ///     Quest では 以下のときに呼ばれるイベントです
+        ///     - ユーザーが外界認識をした
+        ///     - ユーザーが HDM を脱いだ
+        ///     - システムのメニューボタンを押した
+        ///     - HMD のトラッキングが失われた
+        /// </summary>
+        public event Action InputFocusAcquired;
+
+        /// <summary>
+        ///     ユーザーがアプリケーションに集中したときに呼ばれます
+        ///     Quest では 以下のときに呼ばれるイベントです
+        ///     - ユーザーが外界認識をやめた
+        ///     - ユーザーが HDM を被った
+        ///     - システムのメニューボタンを閉じた
+        ///     - HMD のトラッキングが戻った
+        /// </summary>
+        public event Action InputFocusLost;
     }
 }
