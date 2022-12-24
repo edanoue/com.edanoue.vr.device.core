4ef4d134d565b67bdb18783003270c6e6bf45161
Minami Tomonobu <minami@edanoue.com>
2022-12-24 11:39:09 +0900

    :zap: FocusState の API を Provide に移動

--- Runtime/Interfaces/IHeadset.cs
+++ Runtime/Interfaces/IHeadset.cs
@@ -12,10 +12,6 @@ namespace Edanoue.VR.Device.Core
         /// </summary>
         public bool IsMounted { get; }
 
-        /// <summary>
-        ///     ユーザーが現在アプリケーションに集中しているかどうかを取得.
-        /// </summary>
-        public bool HasApplicationFocus { get; }
 
         /// <summary>
         /// </summary>
@@ -24,25 +20,5 @@ namespace Edanoue.VR.Device.Core
         /// <summary>
         /// </summary>
         public event Action Unmounted;
-
-        /// <summary>
-        ///     ユーザーがアプリケーションに集中しなくなったときに呼ばれます
-        ///     Quest では 以下のときに呼ばれるイベントです
-        ///     - ユーザーが外界認識をした
-        ///     - ユーザーが HDM を脱いだ
-        ///     - システムのメニューボタンを押した
-        ///     - HMD のトラッキングが失われた
-        /// </summary>
-        public event Action ApplicationFocusAcquired;
-
-        /// <summary>
-        ///     ユーザーがアプリケーションに集中したときに呼ばれます
-        ///     Quest では 以下のときに呼ばれるイベントです
-        ///     - ユーザーが外界認識をやめた
-        ///     - ユーザーが HDM を被った
-        ///     - システムのメニューボタンを閉じた
-        ///     - HMD のトラッキングが戻った
-        /// </summary>
-        public event Action ApplicationFocusLost;
     }
 }
--- Runtime/Interfaces/IProvider.cs
+++ Runtime/Interfaces/IProvider.cs
@@ -1,5 +1,7 @@
 // Copyright Edanoue, Inc. All Rights Reserved.
 
+using System;
+
 namespace Edanoue.VR.Device.Core
 {
     /// <summary>
@@ -30,5 +32,30 @@ namespace Edanoue.VR.Device.Core
         ///     Get or set display refresh rate
         /// </summary>
         public float RefreshRate { get; set; }
+
+        /// <summary>
+        ///     ユーザーが現在アプリケーションに集中しているかどうかを取得.
+        /// </summary>
+        public bool HasApplicationFocus { get; }
+
+        /// <summary>
+        ///     ユーザーがアプリケーションに集中しなくなったときに呼ばれます
+        ///     Quest では 以下のときに呼ばれるイベントです
+        ///     - ユーザーが外界認識をした
+        ///     - ユーザーが HDM を脱いだ
+        ///     - システムのメニューボタンを押した
+        ///     - HMD のトラッキングが失われた
+        /// </summary>
+        public event Action ApplicationFocusAcquired;
+
+        /// <summary>
+        ///     ユーザーがアプリケーションに集中したときに呼ばれます
+        ///     Quest では 以下のときに呼ばれるイベントです
+        ///     - ユーザーが外界認識をやめた
+        ///     - ユーザーが HDM を被った
+        ///     - システムのメニューボタンを閉じた
+        ///     - HMD のトラッキングが戻った
+        /// </summary>
+        public event Action ApplicationFocusLost;
     }
 }
