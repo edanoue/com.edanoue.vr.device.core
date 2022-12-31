3853d6d404fbdc6fdfc32a72b37ac23a79000e1a
Minami Tomonobu <minami@edanoue.com>
2022-12-31 10:58:09 +0900

    :art: コメントを追加

--- Runtime/Interfaces/Vibration/ISupportedTriggerVibration.cs
+++ Runtime/Interfaces/Vibration/ISupportedTriggerVibration.cs
@@ -11,8 +11,8 @@ namespace Edanoue.VR.Device.Core
         ///     確認済みの対応機種
         ///     - Meta Quest Pro Controller
         /// </remarks>
-        /// <param name="frequency"></param>
-        /// <param name="amplitude"></param>
+        /// <param name="frequency">周波数, 振動の間隔, 期待する範囲は [0, 1]</param>
+        /// <param name="amplitude">振幅, 振動の強さ,期待する範囲は [0, 1]</param>
         void SetVibration(float frequency, float amplitude);
     }
 }
