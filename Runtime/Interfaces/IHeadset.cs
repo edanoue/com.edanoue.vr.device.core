9fca3bf446251e9ba06d1aeb95775d7059af65e3
Minami Tomonobu <minami@edanoue.com>
2023-04-20 12:18:19 +0900

    :sparkles: Add Hmd color control

--- Runtime/Interfaces/IHeadset.cs
+++ Runtime/Interfaces/IHeadset.cs
@@ -4,6 +4,33 @@ using System;
 
 namespace Edanoue.VR.Device.Core
 {
+    /// <summary>
+    /// </summary>
+    public abstract class HeadsetDisplayColorHandler
+    {
+        /// <summary>
+        /// Set display color scale
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="z"></param>
+        /// <param name="w"></param>
+        public abstract void SetColorScale(float x, float y, float z, float w);
+
+        /// <summary>
+        /// Set display color offset
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="z"></param>
+        /// <param name="w"></param>
+        public abstract void SetColorOffset(float x, float y, float z, float w);
+
+        public abstract void ResetColorScale();
+
+        public abstract void ResetColorOffset();
+    }
+
     public interface IHeadset : ITracker
     {
         /// <summary>
@@ -12,6 +39,7 @@ namespace Edanoue.VR.Device.Core
         /// </summary>
         public bool IsMounted { get; }
 
+        public HeadsetDisplayColorHandler DisplayColor { get; }
 
         /// <summary>
         /// </summary>
